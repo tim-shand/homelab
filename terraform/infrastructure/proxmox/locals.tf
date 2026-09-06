4180dbfbe5f156f7af912600f74d08847602b4ff
Tim Shand <tim@tshand.com>
2026-09-06 22:08:53 +1200

    fix: Corrected invalid object attribute in locals.

--- terraform/infrastructure/proxmox/locals.tf
+++ terraform/infrastructure/proxmox/locals.tf
@@ -4,9 +4,10 @@
 # Used to target specific resources to production nodes in the cluster, such as SDN zones.
 
 locals {
+  # Used to limit deployment of resources to nodes missing.
   pve_nodes_production = [
     for k,v in var.pve_nodes : # For key and value in variable.
     v.hostname # Insert value.hostname into new list.
-    if v.enabled # Only if value.enabled = true.
+    if v.production # Only if value.enabled = true.
   ]
 }
