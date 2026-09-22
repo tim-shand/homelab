ca3d5d01fd3f87ab35c4bab703fc8d84a7ab88d9
Tim Shand <tim@tshand.com>
2026-09-22 15:53:05 +1200

    chore: Removed unused code from locals file.

--- terraform/platform/proxmox-cluster/locals.tf
+++ terraform/platform/proxmox-cluster/locals.tf
@@ -2,16 +2,6 @@
 
 # Production Proxmox Nodes
 # Used to target specific resources to production nodes in the cluster, such as SDN zones.
-
-# locals {
-#   # Used to limit deployment of resources to nodes missing.
-#   pve_nodes_production = [
-#     for k,v in var.pve_nodes : # For key and value in variable.
-#     v.node_name # Insert value.hostname into new list.
-#     if v.production # Only if value.enabled = true.
-#   ]
-# }
-
 locals {
   # Used to limit deployment of resources to nodes missing.
   pve_nodes_production = [
