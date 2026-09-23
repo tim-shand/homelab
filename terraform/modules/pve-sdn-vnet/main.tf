70def22ed201c2b7a3ff7f3d4fce01212f3dd998
Tim Shand <tim@tshand.com>
2026-09-23 15:50:10 +1200

    feat: Added outputs for resources from module output.

--- terraform/modules/pve-sdn-vnet/main.tf
+++ terraform/modules/pve-sdn-vnet/main.tf
@@ -12,7 +12,7 @@ terraform {
   }
 }
 
-# Management (VLAN10) --------------------------------- #
+# Proxmox: VNet --------------------------------- #
 resource "proxmox_sdn_vnet" "main" {
   id            = var.vnet_id        # Max 8 characters, no symbols.
   zone          = var.zone_id        # Zone ID from above.
@@ -22,6 +22,7 @@ resource "proxmox_sdn_vnet" "main" {
   vlan_aware    = var.vlan_aware     # Disable for VNet level tagging. Enables vlan-aware on interface, requiring configuration in the guest. 
 }
 
+# Proxmox: Subnet --------------------------------- #
 resource "proxmox_sdn_subnet" "main" {
   for_each        = var.subnets
   vnet            = var.vnet_id               # VNet ID for target/parent VNet.
--- terraform/platform/proxmox-cluster/outputs.tf
+++ terraform/platform/proxmox-cluster/outputs.tf
@@ -4,3 +4,18 @@ output "pve_groups" {
         for k, v in module.pve_iam_group_acls : k => v.group_acls
     }
 }
+
+output "pve_users_sensitive" {
+    description = "Output of all created Proxmox users, including sensitive values."
+    sensitive = true
+    value = {
+        for k, v in module.pve_iam_users_svc : k => v.user_all
+    }
+}
+
+output "pve_users_safe" {
+    description = "Output of all created Proxmox users, safe values only."
+    value = {
+        for k, v in module.pve_iam_users_svc : k => v.user_safe
+    }
+}
